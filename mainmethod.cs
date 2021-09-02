980ff013c743c74b664e67028c27cdbbb9f3ac2b
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-02 14:05:56 +0530

    adding INTERFACE PROGRAM AND MAIN METHOD

---
+++ mainmethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INTERFACEProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig();  // Create a Pig object
+            myPig.animalSound();
+            Console.Read();
+        }
+    }
+}
